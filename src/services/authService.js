806b812704f7bc81effe65456bb2c6455adaf957
Aniket Thakre <aniketthakre2005@gmail.com>
2026-05-19 11:49:28 +0530

    Bug fixed for booking appointment from doctors list page

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,13 +1,13 @@
 import apiClient from "../api/apiClient";
 
 export const login = async (data) => {
-  const response = await apiClient.post("/login", data);
+  const response = await apiClient.post("/api/auth/login", data);
   console.log("here in login function ", response.data);
   return response.data;
 };
 
 export const register = async (data) => {
-  const response = await apiClient.post("/register", data);
+  const response = await apiClient.post("/api/auth//register", data);
   return response.data;
 };
 
